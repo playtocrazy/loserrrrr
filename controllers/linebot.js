7ced850190fe34630d88d5caeef4fb508ebeaab4
richard.lin <richard.lin@104.com.tw>
2017-02-07 16:27:24 +0800

    update

--- controllers/linebot.js
+++ controllers/linebot.js
@@ -8,11 +8,13 @@ exports.createWebhook = function(app, server){
     var bot = LINEBot.create({
         channelID: '1498715301',
         channelSecret: 'd54920ce7dae98a511345961e24d546f',
-        channelToken: 'CnGhAmpI1I6GuSvSY9jeLQ0oacUm+OWz3VYeSRQ3Uc5mqbPtkYVSEyuJkJXF83gJG+ad+aWHl0/TGuKsplhTaea6y+YaSzWL0rdZ1KB9kCQ7Hyw0vvTYREyUlL/pIfWJe9muqwsddAMzhBaCj/B+lAdB04t89/1O/w1cDnyilFU='
+        channelToken: '/SXbu0ym5ciFMOst7N5/Lw1vHUUbcpNFHjMH3wHHl9Pv7hTANjzyKO+q7H4A6UuAG+ad+aWHl0/TGuKsplhTaea6y+YaSzWL0rdZ1KB9kCTS2S43oUjc+yhS+1I3gz2WBe5cIbFw/tL4zXdLp9RcUQdB04t89/1O/w1cDnyilFU='
     }, server);
     app.use(bot.webhook('/webhook'));
+    console.log("bind webhook success!");
 
     bot.on(LINEBot.Events.MESSAGE, function (replyToken, message) {
+        console.log(message);
         if(message === "B"){
             request('https://za8601p1g2.execute-api.us-west-2.amazonaws.com/prod/getcurrentdata', function (error, response, body) {
                 if (!error && response.statusCode == 200) {
