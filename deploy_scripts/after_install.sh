0ef3dabea7885404ff9e2e82df1137052dd03c9f
richard.lin <richard.lin@104.com.tw>
2017-02-14 19:53:48 +0800

    update

--- deploy_scripts/after_install.sh
+++ deploy_scripts/after_install.sh
@@ -2,4 +2,4 @@
 chmod -R 755 /home/ubuntu/project/loserrrrr/
 chown -R ubuntu:ubuntu /home/ubuntu/project/loserrrrr/
 cd /home/ubuntu/project/loserrrrr/
-aws s3 cp public/ s3://loserrrrr-static --recursive
+#aws s3 cp public/ s3://loserrrrr-static --recursive
